129249cb3bf51ca6b8bc65db6050f2c3c25dc1ef
dgutper960 <dgutper960@outlook.es>
2023-11-17 07:49:09 +0100

    js_1

---
+++ intriduccion-javascript/main.js
@@ -0,0 +1,86 @@
+/**
+ * Mostramos un jemplo de alert()
+ */
+alert('Texto del alert');
+
+/**
+ * Creamos variables
+ */
+var nombre = 'David';
+var fecha_creacion = '17/11/23';
+
+/**
+ * Podemos imprimir en el HTML con document.write
+ */
+document.write(`Este documento fue creado por ${nombre} el día ${fecha_creacion}`);
+// de este forma escribimos sin más en el HTML. -> No tenemos control sobre dónde se escribe
+
+/**
+ * Para solucionar esto vamos a crear en el HTML unos div con sus respectivos id
+ * -> Igualamos a una variable un getElemntById
+ */
+
+var datos = document.getElementById("datos");
+// Una vez seleccionado el div mediante id podemos escribir en el
+// Si en ese div hay otro contenido y no queremos que lo machaque, debemos añadir el nuevo y no igualarlo (operador +=)
+datos.innerHTML += `Primer uso de innerHTML por ${nombre} el día ${fecha_creacion} `;
+// observamos que con el uso de innerHTML en el div no se muestra el p HTML que había en ese div (si usamos el operador = )
+
+/**
+ * en un innerHTML podemos poner todo el codigo HTML que queramos entre comillas invertidas
+ */
+var n1 = 'Agustín';
+var n2 = 'Manuela';
+var n3 = 'Josefina';
+var e1 = 29;
+var e2 = 17;
+var e3 = 30;
+
+var datos2 = document.getElementById("datos2");
+datos2.innerHTML = `
+<h1>Hola soy David</h1>
+<p>Esto es el parrafo de ${nombre}</p>
+`;
+
+/**
+ * Una vez capturado el elemento HTML en una variable 
+ * podemos usaar lógica para imprimir en según qué casos
+ */
+if(e1 <= 30){
+    datos2.innerHTML += `<p>${e1} es menor a la edad mínima (30)</p>`;
+}else{
+    datos2.innerHTML += `<p>El usuario ${e1} puede pasar</p>`;
+}
+
+/**
+ * Creamos una tabla con innerHTML
+ */
+var tabla = document.getElementById("tabla");
+tabla.innerHTML += `
+
+<h1>Lista</h1>
+
+<table border="1px">
+<tr>
+  <th>Nombre</th>
+  <th>Edad</th>
+</tr>
+<tr>
+  <td>${n1}</td>
+  <td>${e1}</td>
+</tr>
+<tr>
+  <td>${n2}</td>
+  <td>${e2}</td>
+</tr>
+<tr>
+  <td>${n3}</td>
+  <td>${e3}</td>
+</tr>
+
+</table>
+
+`;
+
+
+
